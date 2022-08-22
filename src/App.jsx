878ba620bb36ffbbef147eed28720124513c3d92
Evgeniy <evgpashko92@gmail.com>
2022-08-22 22:57:08 +0700

    Fix some styles

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,19 @@ import styles from "./style.js";
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth} bg-black`}>
+      <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth} bg-black`}>
+      <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth} bg-black`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
         <Billing />
